8b4c598540e6bb7f160dc2f825e5c328771bdfc2
Shreykapoor17 <shreykapoor17012@gmail.com>
2026-09-21 16:55:31 +0530

    comments modified

--- nonoverlappingpalindromesubsting.cpp
+++ nonoverlappingpalindromesubsting.cpp
@@ -17,7 +17,7 @@ public:
                         pal[i][j] = true;
                     }
 
-                    // Length >= 3
+                    
                     else {
                         pal[i][j] = pal[i + 1][j - 1];
                     }
@@ -25,23 +25,22 @@ public:
             }
         }
 
-        // dp[i] = maximum number of valid palindromes
-        // using the first i characters
+       
         vector<int> dp(n + 1, 0);
 
-        // Step 2: Find maximum number of non-overlapping palindromes
+        
         for (int i = 1; i <= n; i++) {
 
-            // Don't use a palindrome ending at i-1
+            
             dp[i] = dp[i - 1];
 
-            // Try every possible starting position
+            
             for (int j = 0; j < i; j++) {
 
-                // Length of substring s[j...i-1]
+                
                 int len = i - j;
 
-                // Check minimum length and palindrome condition
+                
                 if (len >= k && pal[j][i - 1]) {
 
                     dp[i] = max(dp[i], dp[j] + 1);
